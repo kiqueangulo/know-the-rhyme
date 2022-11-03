06cf189fab549177f6349e0bd000f58539c8c63b
kiqueangulo <kiqueangulo@gmail.com>
2022-11-03 00:16:33 -0400

    Bug with tailwind
    The style is not being rendered

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  darkMode: false,
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
